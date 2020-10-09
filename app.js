4e1d259a53282250097c072d90527447940fa10a
davood rafiee <trstdavood@gmail.com>
2020-10-09 14:30:15 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 setInterval( function() {
-const heartBtn = document.querySelector('button.wpO6b');
+const heartBtn = document.querySelector('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button');
 const heartSvg = heartBtn.querySelector('svg');
 if (heartSvg.getAttribute('aria-label') === 'Like') {
     heartBtn.click();
